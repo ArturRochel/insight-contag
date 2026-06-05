59ed624df39d0e9c7a5631e29e3aa3e8a3204f18
arturrochel <arturrochel@gmail.com>
2026-06-05 10:31:58 -0300

    Limpeza e padronização de colunas no documento processor.py

--- core/processor.py
+++ core/processor.py
@@ -12,6 +12,7 @@ def processar_dados(data_raw: pd.DataFrame) -> pd.DataFrame:
     """
     df = data_raw.drop(columns=0)
 
+    # Renomeia as colunas 
     df = df.rename(columns={
         "Data": "data",
         "Hora do Contato": "hora_do_contato",
@@ -26,4 +27,17 @@ def processar_dados(data_raw: pd.DataFrame) -> pd.DataFrame:
         "Observação": "observacao"
     })
 
-    
+    # Padroniza os tipos de dados de acordo com a coluna
+    df["data"] = pd.to_datetime(df["data"], dayfirst=True, errors="coerce")
+    df[["hora_do_contato", "hora_inicio", "hora_fim", "tempo_decorrido"]] = df[["hora_do_contato", "hora_inicio", "hora_fim", "tempo_decorrido"]].apply(lambda x: pd.to_timedelta(x, errors="coerce"))
+
+    # Adiciona a coluna que calcula o tempo de espera
+    df["tempo_de_espera"] = df["hora_inicio"] - df["hora_do_contato"]
+
+    # Padroniza labels de atendimento
+    # todo - Utilizar o applu: lambda para padronizar todos os campos de label
+    df["demanda_assunto"] = df["demanda_assunto"].str.strip().str.capitalize()
+    df["status"] = df["status"].str.strip().str.capitalize()
+
+    # Limpeza de linhas
+    df = df.dropna(subset=[["hora_do_contato", "hora_inicio", "hora_fim"]])
